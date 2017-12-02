5244b55096d8c4dd8d6475d7d5a35956f5cd7dbf
Kazumasa Kohtaka <kkohtaka@gmail.com>
2017-12-02 18:29:31 +0900

    fixup! Add Grafana

--- terraform/modules/grafana/main.tf
+++ terraform/modules/grafana/main.tf
@@ -40,6 +40,7 @@ resource "google_compute_instance_template" "grafana" {
   }
 
   disk {
+    device_name = "grafana"
     source = "${google_compute_disk.grafana.name}"
     type   = "PERSISTENT"
     auto_delete = false
